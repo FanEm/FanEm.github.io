22da23be62dafdbfe110a416f8ee99fbf56f0eca
FanEm <NAI90@YA.RU>
2016-12-09 18:16:26 +0300

    Dropdown

--- js/guess.js
+++ js/guess.js
@@ -1,38 +1,17 @@
 /**
  * Created by fanem on 30.11.16.
  */
-
 function playGuess() {
     var answer = parseInt(Math.random() * 100);
     var count = 0;
-    var userAnswer = document.getElementById('usrNumber').value;
-    userAnswer = parseInt(userAnswer);
-
-    if (userAnswer > answer) {
-        alert('Too much');
-        count++;
-    } else if (userAnswer < answer) {
-        alert('Too low');
-        count++;
-    } else if (userAnswer == answer) {
-        count++;
-        alert('You are right. You guessed using ' + count + ' attempts');
-        break;
-    } else {
-        alert('You must enter the number');
-    }
-/*    userAnswer = parseInt(userAnswer);*/
-
-
-    /*   while (true) {
-        var userAnswer = document.getElementById("usrNumber");
-        userAnswer.innerHTML;
-       if (userAnswer == null) {
+    while (true) {
+        var userAnswer = prompt('Enter the number from 1 to 100');
+        if (userAnswer == null) {
             break;
         }
         userAnswer = parseInt(userAnswer);
 
-        /*if (userAnswer > answer) {
+        if (userAnswer > answer) {
             alert('Too much');
             count++;
         } else if (userAnswer < answer) {
@@ -45,6 +24,6 @@ function playGuess() {
         } else {
             alert('You must enter the number');
         }
-*/
+
     }
 }
