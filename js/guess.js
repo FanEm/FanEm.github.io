89efd5a34086396ab836ac8913cc76f176c835be
FanEm <NAI90@YA.RU>
2016-12-09 18:55:59 +0300

    modal window in guess

--- js/guess.js
+++ js/guess.js
@@ -1,29 +1,49 @@
 /**
  * Created by fanem on 30.11.16.
  */
+
 function playGuess() {
     var answer = parseInt(Math.random() * 100);
     var count = 0;
-    while (true) {
-        var userAnswer = prompt('Enter the number from 1 to 100');
-        if (userAnswer == null) {
-            break;
-        }
-        userAnswer = parseInt(userAnswer);
+    var userAnswer = document.getElementById('usrNumber').value;
+    userAnswer = parseInt(userAnswer);
 
-        if (userAnswer > answer) {
-            alert('Too much');
-            count++;
-        } else if (userAnswer < answer) {
-            alert('Too low');
-            count++;
-        } else if (userAnswer == answer) {
-            count++;
-            alert('You are right. You guessed using ' + count + ' attempts');
-            break;
-        } else {
-            alert('You must enter the number');
-        }
-
+    if (userAnswer > answer) {
+        alert('Too much');
+        count++;
+    } else if (userAnswer < answer) {
+        alert('Too low');
+        count++;
+    } else if (userAnswer == answer) {
+        count++;
+        alert('You are right. You guessed using ' + count + ' attempts');
+        break;
+    } else {
+        alert('You must enter the number');
     }
+    /*    userAnswer = parseInt(userAnswer);*/
+
+
+    /*   while (true) {
+     var userAnswer = document.getElementById("usrNumber");
+     userAnswer.innerHTML;
+     if (userAnswer == null) {
+     break;
+     }
+     userAnswer = parseInt(userAnswer);
+
+     /*if (userAnswer > answer) {
+     alert('Too much');
+     count++;
+     } else if (userAnswer < answer) {
+     alert('Too low');
+     count++;
+     } else if (userAnswer == answer) {
+     count++;
+     alert('You are right. You guessed using ' + count + ' attempts');
+     break;
+     } else {
+     alert('You must enter the number');
+     }
+     */
 }
